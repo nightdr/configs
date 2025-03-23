67a1ba030fb7c707b4bd90936692bffd54b6613b
David Randall <madmathskills@gmail.com>
2025-03-22 19:41:24 -0600

    added more neovim config: configured telescope, added autocomplete, lsp, indentation, todo highlighting, status bar, highlight on yank, and escape to cancel highlighting after search

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
+-- Global variables
+
 -- Leaders
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = false
 
 -- Line numbers
 vim.opt.number = true
@@ -10,11 +14,30 @@ vim.opt.mouse = "a"
 
 vim.opt.background = "light"
 
+-- Disable showing the mode in the status line since we're using a status line plugin
+vim.opt.showmode = false
+
 -- Indentation (see https://stackoverflow.com/a/51995699)
 vim.opt.tabstop = 4
 vim.opt.softtabstop = -1
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
+
+-- Keymaps
+-- Have escape remove highlight after searching
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Auto commands
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
 
 require("config.lazy")
 
---
+++ nvim/lua/plugins/miscellaneous.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "tpope/vim-sleuth",
+    },
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            -- surround motions
+            require("mini.surround").setup()
+
+            local statusline = require "mini.statusline"
+
+            statusline.setup { use_icons = vim.g.have_nerd_font }
+            -- You can configure sections in the statusline by overriding their
+            -- default behavior. For example, here we set the section for
+            -- cursor location to LINE:COLUMN
+            ---@diagnostic disable-next-line: duplicate-set-field
+            statusline.section_location = function()
+                return "%2l:%-2v"
+            end
+        end,
+    },
+    {
+        "folke/todo-comments.nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = { signs = false }
+    },
+}
