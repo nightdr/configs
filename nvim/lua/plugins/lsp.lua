8aab2e20ea9eb409d41254c7b54da24b563d41f6
David Randall <madmathskills@gmail.com>
2025-03-23 13:23:55 -0600

    removed diagnostics and highlight on cursor stop from lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -121,35 +121,6 @@ return {
                         end
                     end
 
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-                        local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-                        vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
-
-                        vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.clear_references,
-                        })
-
-                        vim.api.nvim_create_autocmd('LspDetach', {
-                            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-                            callback = function(event2)
-                                vim.lsp.buf.clear_references()
-                                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-                            end,
-                        })
-                    end
-
                     -- The following code creates a keymap to toggle inlay hints in your
                     -- code, if the language server you are using supports them
                     --
@@ -164,6 +135,9 @@ return {
 
             -- Diagnostic Config
             -- See :help vim.diagnostic.Opts
+            -- Disable diagnostics
+            vim.diagnostic.disable()
+            --[[
             vim.diagnostic.config {
                 severity_sort = true,
                 float = { border = 'rounded', source = 'if_many' },
@@ -176,6 +150,8 @@ return {
                         [vim.diagnostic.severity.HINT] = '󰌶 ',
                     },
                 } or {},
+                -- Removes virtual text (warning descriptions, etc.)
+                virtual_text = false,
                 virtual_text = {
                     source = 'if_many',
                     spacing = 2,
@@ -190,6 +166,7 @@ return {
                     end,
                 },
             }
+            --]]
 
             -- LSP servers and clients are able to communicate to each other what features they support.
             --  By default, Neovim doesn't support everything that is in the LSP specification.
