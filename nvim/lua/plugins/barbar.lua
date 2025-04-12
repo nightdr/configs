2ede72db7f85ef48cda33cbf1b19a7fb947bb1af
David Randall <madmathskills@gmail.com>
2025-04-12 13:13:49 -0600

    added barbar to neovim config and added lazy-lock.json to gitignore

---
+++ nvim/lua/plugins/barbar.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "romgrk/barbar.nvim",
+        init = function ()
+            vim.g.barbar_auto_setup = false
+
+            local map = function(keys, func)
+                vim.keymap.set("n", keys, func, { noremap = true, silent = true })
+            end
+
+            -- Move to previous/next buffer
+            map("<A-h>", "<Cmd>BufferPrevious<CR>")
+            map("<A-l>", "<Cmd>BufferNext<CR>")
+
+            -- Close buffer
+            map("<A-w>", "<Cmd>BufferClose<CR>")
+
+        end,
+        opts = {
+            icons = {
+                filetype = {
+                    enabled = false,
+                },
+            },
+        },
+    },
+}
